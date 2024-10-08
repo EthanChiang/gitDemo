548fcf5a8b4ef2e1ef1ab67761a718b44baf52d9
ethan.chiang <ethan0514@gmail.com>
2024-10-08 11:19:19 +0800

    測試amend功能

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
          //This is the First Version of the Code
+
+        //This is second commit use command line
+        
     }
 }
